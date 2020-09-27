05be73491e6b6733d4dac925edd0c1c814fdf4c2
Kateryna Kalinina <katerynakalinina7@gmail.com>
2020-09-28 01:18:55 +0300

    homework19

---
+++ script.js
@@ -0,0 +1,54 @@
+window.addEventListener('mousemove', (e) => {
+    const horizontal = document.querySelector('#lineX');
+    horizontal.classList.add('horizontal');
+    const vertical = document.querySelector('#lineY');
+    vertical.classList.add('vertical');
+    let x = e.clientX;
+    let y = e.clientY;
+    horizontal.style.top = `${y}px`;
+    vertical.style.left = `${x}px`;
+
+});
+
+
+const highlight = (e) => {
+    const highlightedArea = document.querySelector('#highlightedArea');
+    highlightedArea.className = 'highlight';
+
+    let clickX = e.clientX;
+    let clickY = e.clientY;
+
+    const mousemoveProcess = (e) => {
+        let moveX = e.clientX;
+        let moveY = e.clientY;
+
+        if (moveX > clickX) {
+            highlightedArea.style.left = `${clickX}px`;
+            highlightedArea.style.right = `${window.innerWidth - moveX}px`;
+        } else {
+            highlightedArea.style.left = `${moveX}px`;
+            highlightedArea.style.right = `${window.innerWidth - clickX}px`;
+        };
+
+
+        if (moveY > clickY) {
+            highlightedArea.style.top = `${clickY}px`;
+            highlightedArea.style.bottom = `${window.innerHeight - moveY}px`;
+        } else {
+            highlightedArea.style.top = `${moveY}px`;
+            highlightedArea.style.bottom = `${window.innerHeight - clickY}px`;
+        };
+
+    };
+
+    window.addEventListener('mousemove', mousemoveProcess);
+
+    window.addEventListener('mouseup', () => {
+        highlightedArea.removeAttribute('style');
+        window.removeEventListener('mousemove', mousemoveProcess);
+    });
+};
+
+window.addEventListener('mousedown', highlight);
+
+// Я думала, что у меня мозг вытечет через уши, когда я делала задачу с выделением :)))
